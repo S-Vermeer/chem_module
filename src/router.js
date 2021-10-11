7ccd096d30c22b7053eca8751b09ea957eb0f92b
Skyler <s.vermeer@fhict.nl>
2021-10-11 09:08:51 +0200

    First setup for Webstorm

---
+++ src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+//IMPORT COMPONENTS
+// import LoginForm from './components/LoginForm.vue';
+// import Molecules from './components/Molecules.vue';
+// import GameLevel from './components/GameLevel.vue';
+// import Elements from './components/Elements.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    // routes: [
+    //     {
+    //         path: '/',
+    //         name: 'Home',
+    //         component: Molecules,
+    //     },
+    //     {
+    //         path: '/login',
+    //         name: 'Login',
+    //         component: LoginForm,
+    //     },
+    //     {
+    //         path: '/game/:formula',
+    //         name: 'Game',
+    //         component: GameLevel,
+    //     },
+    //     {
+    //         path: '/elements',
+    //         name: 'Elements',
+    //         component: Elements,
+    //     },
+    // ],
+});
